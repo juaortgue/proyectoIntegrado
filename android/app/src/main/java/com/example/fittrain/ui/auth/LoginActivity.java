bcf6e8e6a5d9eca433b5776f23021dec90ea6db1
juan antonio <juanantonioortizguerra@gmail.com>
2019-03-12 13:02:22 +0100

    se trabaja en login

--- android/app/src/main/java/com/example/fittrain/ui/auth/LoginActivity.java
+++ android/app/src/main/java/com/example/fittrain/ui/auth/LoginActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.fittrain.R;
@@ -16,10 +17,41 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signInFragment = new SignInFragment();
-        signUpFragment = new SignUpFragment();
-        fragmentChanger = getSupportFragmentManager().beginTransaction().replace(R.id.containerLogin, signInFragment);
-        fragmentChanger.commit();
+        loadItems();
+        loadDefaultFragment();
+
 
     }
+    public void loadItems(){
+        //btnGoSignIn = findViewById(R.id.);
+        /*btnGoSignUp = findViewById(R.id.buttonGoRegister);
+        btnSignIn = findViewById(R.id.buttonLogin);*/
+        //btnSignUp = ;
+        signInFragment = new SignInFragment();
+        signUpFragment = new SignUpFragment();
+
+        /*btnGoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignUp();
+            }
+        });*/
+        /*btnGoSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignIn();
+            }
+        });*/
+    }
+    public void loadDefaultFragment(){
+        fragmentChanger = getSupportFragmentManager().beginTransaction().replace(R.id.containerLogin, signInFragment);
+        fragmentChanger.commit();
+    }
+    public void goSignUp(){
+
+    }
+    public void goSignIn(){
+
+    }
+
 }
--- android/app/src/main/java/com/example/fittrain/ui/auth/SignInFragment.java
+++ android/app/src/main/java/com/example/fittrain/ui/auth/SignInFragment.java
@@ -1,11 +1,14 @@
 package com.example.fittrain.ui.auth;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.fittrain.R;
 
@@ -13,8 +16,10 @@ import com.example.fittrain.R;
  * A simple {@link Fragment} subclass.
  */
 public class SignInFragment extends Fragment {
-
-
+    private FragmentTransaction fragmentChanger;
+    private Fragment signUpFragment;
+    private Button btnSignIn, btnGoSignUp, btnSignUp, btnGoSignIn;
+    private Context ctx;
     public SignInFragment() {
         // Required empty public constructor
     }
@@ -24,7 +29,25 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View v= inflater.inflate(R.layout.fragment_sign_in, container, false);
+        loadItems(v);
+        return v;
+    }
+    public void loadItems(View v){
+        ctx=getContext();
+        signUpFragment = new SignUpFragment();
+        btnGoSignUp = v.findViewById(R.id.buttonGoRegister);
+        btnSignIn = v.findViewById(R.id.buttonLogin);
+
+
+        btnGoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentChanger = getFragmentManager().beginTransaction().replace(R.id.containerLogin, signUpFragment);
+                fragmentChanger.commit();
+            }
+        });
+
     }
 
 }
