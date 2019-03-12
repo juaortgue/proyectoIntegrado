0f630a30cf413edc07bb56be8c4a8cb247add441
juan antonio <juanantonioortizguerra@gmail.com>
2019-03-12 10:55:17 +0100

    model category

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import user from './user'
 import auth from './auth'
 import gym from './gym'
+import category from './category'
 
 const router = new Router()
 
@@ -31,5 +32,6 @@ const router = new Router()
 router.use('/users', user)
 router.use('/auth', auth)
 router.use('/gyms', gym)
+router.use('/categories', category)
 
 export default router
