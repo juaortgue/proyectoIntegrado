22ff05ff4aebba734c520953613bee4cdbf94bf7
juan antonio ortiz guerra <juanantonioortizguerra@gmail.com>
2019-03-17 14:05:29 +0100

    se cambia imgur client id

--- api/src/services/upload/index.js
+++ api/src/services/upload/index.js
@@ -1,7 +1,7 @@
 
 const imgur = require('imgur')
-imgur.setClientId('55eb55cd14ff95d')
-
+//imgur.setClientId('55eb55cd14ff95d')
+imgur.setClientId('954ded56c7a5a96')
 export function uploadFromBinary (binary) {
   let base64 = Buffer.from(binary).toString('base64')
   return imgur.uploadBase64(base64) // Devuelve una promesa
